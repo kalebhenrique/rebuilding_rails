9a10f63b5dd48bfad5a1810f394849c12bb5d993
kalebhenrique <kalebhenriquebr@gmail.com>
2025-12-25 21:10:32 -0300

    Add routing system

--- lib/rebuilding_rails.rb
+++ lib/rebuilding_rails.rb
@@ -6,9 +6,20 @@ module RebuildingRails
   class Error < StandardError; end
 
   class Application
-    def call(_env)
+    def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, { 'content-type' => 'text/html' },
-       ['Hello from Rebuilding Rails']]
+       [text]]
     end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    attr_reader :env
   end
 end
---
+++ lib/rebuilding_rails/routing.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module RebuildingRails
+  class Application
+    def get_controller_and_action(env)
+      _, cont, action, =
+        env['PATH_INFO'].split('/', 4)
+      cont = cont.capitalize
+      cont += 'Controller'
+
+      [Object.const_get(cont), action]
+    end
+  end
+end
